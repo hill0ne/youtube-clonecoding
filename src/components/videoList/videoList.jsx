5b3daddeee0f804330389f890c9dddcae538abbf
hill0ne <jeanj531@gmail.com>
2022-04-12 19:18:34 +0900

    [수정] PostCSS를 사용하여 스타일 모듈화 적용

--- src/components/videoList/videoList.jsx
+++ src/components/videoList/videoList.jsx
@@ -1,18 +1,16 @@
 import VideoItem from "../videoItem/videoItem";
-import "./videoList.css";
+import styles from "./videoList.module.css";
 
 // 부모 컴포넌트의 속성을 받아 ,
 // 부모가 가진 videos 를 재구성하고
 // 재구성한 video를 VideoItem 컴포넌트의 video 속성에 정의한다.
 function VideoList(props) {
   return (
-    <section>
-      <ul className="contaier-videoList">
-        {props.videos.map((video) => (
-          <VideoItem video={video} key={video.id} />
-        ))}
-      </ul>
-    </section>
+    <ul className={styles.container}>
+      {props.videos.map((video) => (
+        <VideoItem video={video} key={video.id} />
+      ))}
+    </ul>
   );
 }
 
