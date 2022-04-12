81e2235c05e6275d39218b4b51fa048686d2f503
hill0ne <jeanj531@gmail.com>
2022-04-12 20:20:04 +0900

    [수정] props 인자 대신 구조분해 할당 사용

--- src/components/videoItem/videoItem.jsx
+++ src/components/videoItem/videoItem.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import styles from "./videoItem.module.css";
 
 // 부모 컴포넌트의 속성을 받고, 그 속성의 title 값을 <h1> 요소 안에 정의한다.
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video: { snippet } }) => (
+  <>
+    <li className={styles.container}>
+      <img
+        src={snippet.thumbnails.medium.url}
+        className={styles.thumbnails}
+        alt="video thumbnails"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.videoTitle}>{snippet.title}</p>
+        <p className={styles.channelTitle}>{snippet.channelTitle}</p>
+      </div>
+    </li>
+  </>
+);
 
 export default VideoItem;
