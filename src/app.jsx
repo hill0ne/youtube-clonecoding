17c01ca5321e220d2cf11b77be217b2adc13ca69
hill0ne <jeanj531@gmail.com>
2022-04-13 17:18:27 +0900

    [수정] 스타일 모듈화 적용, 접근성 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./components/header/header.css";
 import Header from "./components/header/header";
 import VideoList from "./components/videoList/videoList";
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import logo from "../../img/logo.png";
 import search from "../../img/search.png";
+import styles from "./header.module.css";
 
 function Header(props) {
+  function toSearch(e) {
+    e.preventDefault();
+    console.log("search");
+  }
+
   return (
-    <header className="youtube-header">
+    <header className={styles.header}>
       <a href="#">
-        <img className="icon-logo" src={logo} alt="youtube" />
+        <img className={styles.logo} src={logo} alt="youtube" />
       </a>
-      <span className="text-logo">YouTube</span>
-      <input type="text" className="input-search" placeholder="Search.." />
-      <span className="container-searchBtn">
-        <button className="button-search">
-          <img className="icon-search" src={search} alt="search" />
+      <span className={styles.logoText} tabIndex="0">
+        YouTube
+      </span>
+      <input type="text" className={styles.search} placeholder="Search.." />
+      <span className={styles.containerBtn} tabIndex="0">
+        <button className={styles.searchBtn} onClick={toSearch}>
+          <img className={styles.searchIcon} src={search} alt="search" />
         </button>
       </span>
     </header>
